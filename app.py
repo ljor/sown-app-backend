d6c692b2adb10336d9a71206f8be68cef7cc7c09
ljor <leah.m.jorgensen@gmail.com>
2021-12-14 12:18:34 -0500

    user register route created

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 import models
+import os
+from flask_cors import CORS
+from flask_login import LoginManager, login_manager
+
+from dotenv import load_dotenv
+load_dotenv()
 
 from resources.seeds import seeds
 from resources.users import users
@@ -8,6 +14,33 @@ DEBUG=True
 PORT=8000
 
 app = Flask(__name__)
+
+app.secret_key = os.environ.get("FLASK_APP_SECRET")
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    try:
+        print("loading the following user")
+        user = models.User.get_by_id(user_id)
+        return user
+    except models.DoesNotExist:
+        return None
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return jsonify(
+        data={
+            'error': 'User not logged in'
+        },
+        message="You must be logged in to ccass that resource",
+        status=401
+    ), 401
+
+CORS(seeds, origins=['http://localhost:3000'], supports_credentials=True)
+CORS(users, origins=['http://localhost:3000'], supports_credentials=True)
 
 app.register_blueprint(seeds, url_prefix='/api/v1/seeds')
 app.register_blueprint(users, url_prefix='/api/v1/users')
--- resources/users.py
+++ resources/users.py
@@ -1,6 +1,8 @@
 import models
 
 from flask import Blueprint, request, jsonify
+from flask_bcrypt import generate_password_hash, check_password_hash
+from flask_login import login_user, current_user, logout_user
 from playhouse.shortcuts import model_to_dict
 
 users = Blueprint('users', 'users')
@@ -16,4 +18,40 @@ def user_test_route():
         'data': user_dicts,
         'message': f"Successfully found {len(user_dicts)} users",
         'status': 200
-    }), 200 
+    }), 200
+
+@users.route('/register', methods=['POST']) 
+def register():
+    payload = request.get_json()
+    payload['email'] = payload['email'].lower()
+    payload['username'] = payload['username'].lower()
+
+    try:
+        models.User.get(models.User.email == payload['email'])
+
+        return jsonify(
+            data={},
+            message=f"A user with the email {payload['email']} already exists",
+            status=401
+        ), 401
+
+    except models.DoesNotExist:
+        pw_hash = generate_password_hash(payload['password'])
+
+        create_user = models.User.create(
+            username=payload['username'],
+            email=payload['email'],
+            zip=payload['zip'],
+            password=pw_hash
+        )
+
+        login_user(create_user)
+
+        created_user_dict = model_to_dict(create_user)
+        created_user_dict.pop('password')
+
+        return jsonify(
+            data=created_user_dict,
+            message=f"Successfully registered {created_user_dict['email']}",
+            status=201
+        ), 201
